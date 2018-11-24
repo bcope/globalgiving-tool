49ea758475ea2efaaefaa4074185e8b7e0239dcf
Aryn Harmon <achcello@gmail.com>
2018-11-24 13:52:26 -0800

    modify gg run to insert things into the database

--- gg/commands/cmd_run.py
+++ gg/commands/cmd_run.py
@@ -1,22 +1,24 @@
-import click, requests
-from gg.db import list_from_db
+import click
+import requests
+from gg.db import list_from_db, upload_data
 from gg.cli import pass_context
 
 
 @click.command("run", short_help="Run a scraper")
-@click.option("--n", nargs=1, required=True, type=str)
+@click.argument("name", required=True, type=str)
 @pass_context
-def cli(ctx, n):
+def cli(ctx, name):
     search = "Finding scraper {} from list of registered scrapers..."
-    ctx.log(search.format(n))
+    ctx.log(search.format(name))
     try:
         scrapers = list_from_db()
-        route = list(filter(lambda scraper: scraper["name"] == str(n), scrapers))[0][
+        route = list(filter(lambda scraper: scraper["name"] == str(name), scrapers))[0][
             "routes"
         ]["Data"]
-        ctx.log("Scraper {} found!".format(n))
+        ctx.log("Scraper {} found!".format(name))
     except StopIteration:
-        ctx.log("Scraper {} not found.".format(n))
+        ctx.log("Scraper {} not found.".format(name))
         return
-    contents = requests.get(route).text
-    ctx.log(contents)
+    contents = requests.get(route).json()
+    result = upload_data(contents)
+    ctx.log(result)
